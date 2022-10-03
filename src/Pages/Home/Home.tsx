f28d3f8643b2256b87ca97c935e8aed40fdbbd35
AZIZUL HAQUE TOUSIF <meta.block.tousif@gmail.com>
2022-10-03 11:56:56 +0600

    Project initialized

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+      <div className="text-center p-28">
+        <h1 className="text-3xl text-lime-800">
+         Welcome To Juice Bar!
+        </h1>
+      </div>
+    );
+};
+
+export default Home;
