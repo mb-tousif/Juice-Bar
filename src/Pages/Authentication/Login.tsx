ff3cb1714cc1c29f465e6262bd9eff43f4429c91
mb-tousif <meta.block.tousif@gmail.com>
2023-01-05 10:35:12 +0600

    react-hook-form implemented

--- src/Pages/Authentication/Login.tsx
+++ src/Pages/Authentication/Login.tsx
@@ -1,43 +1,83 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+
+type FormValues = {
+  email: string;
+  password: string;
+};
 
 export default function Login() {
-    const handleSubmit = (e: React.SyntheticEvent) =>{
-        e.preventDefault();
-    }
+    const { register, formState: { errors }, handleSubmit } = useForm<FormValues>();
+  const navigate = useNavigate();
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    navigate("/store")
+  };
   return (
     <div className="bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED] pt-8 pb-8 flex justify-center">
       <div className="block p-6 rounded-lg shadow-lg sm:w-3/5 bg-white max-w-sm">
-        <h1 className="text-2xl font-bold mb-4 text-center text-lime-800">Please Login Here</h1>
-        <form onSubmit={handleSubmit}>
+        <h1 className="text-2xl font-bold mb-4 text-center text-lime-800">
+          Please Login Here
+        </h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-6">
-            <input type="email" className="block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED] bg-clip-padding border-none rounded transition ease-in-out m-0 placeholder-teal-900" placeholder="Email address" />
+            <input
+              type="email"
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: "Email is required",
+                },
+                pattern: {
+                  value:
+                    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                  message: "Please enter valid email address",
+                },
+              })}
+              className="block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED] bg-clip-padding border-none rounded transition ease-in-out m-0 placeholder-teal-900"
+              placeholder="Email address"
+            />
+            {errors.email?.type === "required" && (
+              <span className="text-red-600">{errors.email.message}</span>
+            )}
+            {errors.email?.type === "pattern" && (
+              <span className="text-red-600">{errors.email.message}</span>
+            )}
           </div>
           <div className="mb-4">
             <input
               type="password"
+              {...register("password", {
+                required: { value: true, message: "Password is required" },
+                minLength: {
+                  value: 6,
+                  message: "Password minimum length should be six characters",
+                },
+              })}
               className="block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED] bg-clip-padding border-none rounded transition ease-in-out m-0 placeholder-teal-900"
               placeholder="Enter your password"
             />
+            {errors.password?.type === "required" && (
+              <span className="text-red-600">{errors.password.message}</span>
+            )}
+            {errors.password?.type === "minLength" && (
+              <span className="text-red-600">{errors.password.message}</span>
+            )}
           </div>
           <div className="mb-2">
             <Link to={"#/"}>Forgot Password?</Link>
           </div>
-          <div className="h-0.5 mb-6 bg-slate-600"></div>
-          <div className="text-center mb-6">
-            <input
-              type="checkbox" id="agree"
-              className="h-4 w-4 border-none rounded-sm bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED] checked:bg-[#022b53] transition duration-200 mt-1 align-top mr-2 cursor-pointer"
-            />
-            <label htmlFor="agree" className="inline-block text-gray-800">I have read and agree to the terms.</label>
-          </div>
-          <button type="submit" className="block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED] rounded transition ease-in-out m-0 placeholder-teal-900">
+          <button
+            type="submit"
+            className="block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED] rounded transition ease-in-out m-0 placeholder-teal-900"
+          >
             Login 🥤
           </button>
         </form>
         <div className="h-0.5 mb-2 mt-6 bg-slate-600"></div>
         <div className="mb-2 text-center text-lg">
-         New to here ? <Link to={"/signIn"}>Please Sign In.</Link>
+          New to here ? <Link to={"/signIn"}>Please Sign In.</Link>
         </div>
       </div>
     </div>
--- src/Pages/Authentication/Register.tsx
+++ src/Pages/Authentication/Register.tsx
@@ -1,53 +1,119 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useForm,SubmitHandler } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+
+type FormValues = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+};
 
 export default function Register() {
-  const handleSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault();
+  const { register, formState: { errors }, handleSubmit } = useForm<FormValues>();
+  const navigate = useNavigate();
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    navigate("/login")
   };
   return (
     <div className="bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED] pt-8 pb-8 flex justify-center">
       <div className="block p-6 rounded-lg shadow-lg bg-white max-w-md">
-        <h1 className="text-2xl font-bold mb-4 text-center text-lime-800">Please Sign In Here</h1>
-        <form onSubmit={handleSubmit}>
+        <h1 className="text-2xl font-bold mb-4 text-center text-lime-800">
+          Please Sign In Here
+        </h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="grid sm:grid-cols-2 grid-cols-1 sm:gap-4">
             <div className="mb-6">
               <input
                 type="text"
+                {...register("firstName", {
+                  required: {
+                    value: true,
+                    message: "First Name is required",
+                  },
+                  minLength: {
+                    value: 3,
+                    message: "error message",
+                  },
+                })}
                 className="block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED] bg-clip-padding border-none rounded transition ease-in-out m-0 placeholder-teal-900"
                 placeholder="First name"
               />
+              {errors.firstName?.type === "required" && (
+                <span className="text-red-600">{errors.firstName.message}</span>
+              )}
+              {errors.firstName?.type === "minLength" && (
+                <span className="text-red-600">{errors.firstName.message}</span>
+              )}
             </div>
             <div className="mb-6">
               <input
                 type="text"
+                {...register("lastName", {
+                  required: {
+                    value: true,
+                    message: "Last Name is required",
+                  },
+                  minLength: {
+                    value: 3,
+                    message: "error message",
+                  },
+                })}
                 className="block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED] bg-clip-padding border-none rounded transition ease-in-out m-0 placeholder-teal-900"
                 placeholder="Last name"
               />
+              {errors.lastName?.type === "required" && (
+                <span className="text-red-600">{errors.lastName.message}</span>
+              )}
+              {errors.lastName?.type === "minLength" && (
+                <span className="text-red-600">{errors.lastName.message}</span>
+              )}
             </div>
           </div>
           <div className="mb-6">
             <input
               type="email"
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: "Email is required",
+                },
+                pattern: {
+                  value:
+                    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                  message: "Please enter valid email address",
+                },
+              })}
               className="block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED] bg-clip-padding border-none rounded transition ease-in-out m-0 placeholder-teal-900"
               placeholder="Email address"
             />
+            {errors.email?.type === "required" && (
+              <span className="text-red-600">{errors.email.message}</span>
+            )}
+            {errors.email?.type === "pattern" && (
+              <span className="text-red-600">{errors.email.message}</span>
+            )}
           </div>
           <div className="mb-6">
             <input
               type="password"
+              {...register("password", {
+                required: { value: true, message: "Password is required" },
+                minLength: {
+                  value: 6,
+                  message: "Password minimum length should be six characters",
+                },
+              })}
               className="block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED] bg-clip-padding border-none rounded transition ease-in-out m-0 placeholder-teal-900"
               placeholder="Password"
             />
-          </div>
-          <div className="text-center mb-6">
-            <input
-              type="checkbox" id="subscribe"
-              className="h-4 w-4 border-none rounded-sm bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED] checked:bg-[#022b53] transition duration-200 mt-1 align-top mr-2 cursor-pointer"
-            />
-            <label htmlFor="subscribe" className="form-check-label inline-block text-gray-800">
-              Subscribe to our newsletter
-            </label>
+            {errors.password?.type === "required" && (
+              <span className="text-red-600">{errors.password.message}</span>
+            )}
+            {errors.password?.type === "minLength" && (
+              <span className="text-red-600">{errors.password.message}</span>
+            )}
           </div>
           <button
             type="submit"
@@ -58,7 +124,7 @@ export default function Register() {
         </form>
         <div className="h-0.5 mb-2 mt-6 bg-slate-600"></div>
         <div className="mb-2 text-center text-lg">
-         Already Sign up ? <Link to={"/login"}>Login now </Link>
+          Already Sign up ? <Link to={"/login"}>Login now </Link>
         </div>
       </div>
     </div>
