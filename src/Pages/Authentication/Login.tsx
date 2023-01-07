810580c8ef7b741859345f14590a93819d6f8712
mb-tousif <meta.block.tousif@gmail.com>
2023-01-07 19:55:25 +0600

    login system implemented

--- src/Pages/Authentication/Login.tsx
+++ src/Pages/Authentication/Login.tsx
@@ -1,19 +1,51 @@
-import React from "react";
+import React,{useState} from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
+import Loader from "../../Components/Loader/Loader";
+import { MdOutlineVisibilityOff, MdOutlineVisibility } from "react-icons/md";
+
 
 type FormValues = {
   email: string;
   password: string;
 };
+interface CreateLogin {
+  email: string;
+  password: string;
+};
 
 export default function Login() {
-    const { register, formState: { errors }, handleSubmit } = useForm<FormValues>();
+  const { register, formState: { errors }, handleSubmit } = useForm<FormValues>();
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const [loader, setLoader]= useState(false);
 
   const onSubmit: SubmitHandler<FormValues> = (data) => {
-    navigate("/store")
-  };
+    const checkUser:CreateLogin ={
+      email: data.email,
+      password: data.password
+    }
+  const url = "https://juice-bar-server.vercel.app/api/v1/login";
+  setLoader(true)
+  fetch(url,{
+    method: "POST",
+    headers: {
+        'content-type': 'application/json'
+    },
+    body: JSON.stringify(checkUser)
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if(data.status ==="success"){
+        setLoader(false)
+        navigate("/store")
+        }
+    });
+};
+
+  if(loader){
+    return <Loader/>
+  }
   
   return (
     <div className="bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED] pt-8 pb-8 flex justify-center">
@@ -46,26 +78,60 @@ export default function Login() {
               <span className="text-red-600">{errors.email.message}</span>
             )}
           </div>
-          <div className="mb-4">
+          {showPassword ?
+          <div className="relative mb-6">
             <input
               type="password"
               {...register("password", {
-                required: { value: true, message: "Password is required" },
-                minLength: {
-                  value: 6,
-                  message: "Enter your sign up Password",
+                required: { value: true, message: "Password is required!" },
+                pattern: {
+                  value:
+                    /^(?=.*[a-z]{3,})(?=.*[A-Z])(?=.*\d)(?=.*[^\w\d])(.{6,})$/,
+                  message: "Enter strong password with minLength six,minNumbers one,minUppercase one, minSymbols one. ",
                 },
               })}
               className="block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED] bg-clip-padding border-none rounded transition ease-in-out m-0 placeholder-teal-900"
-              placeholder="Enter your password"
+              placeholder="Password"
             />
             {errors.password?.type === "required" && (
               <span className="text-red-600">{errors.password.message}</span>
             )}
-            {errors.password?.type === "minLength" && (
+            {errors.password?.type === "pattern" && (
               <span className="text-red-600">{errors.password.message}</span>
             )}
+            <MdOutlineVisibilityOff
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute text-slate-700 right-0 top-[6px] mr-4 cursor-pointer"
+              size={28}
+            />
+          </div>:
+          <div className="relative mb-6">
+            <input
+              type="text"
+              {...register("password", {
+                required: { value: true, message: "Password is required!" },
+                pattern: {
+                  value:
+                    /^(?=.*[a-z]{3,})(?=.*[A-Z])(?=.*\d)(?=.*[^\w\d])(.{6,})$/,
+                  message: "Enter strong password with minLength six,minNumbers one,minUppercase one, minSymbols one. ",
+                },
+              })}
+              className="block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED] bg-clip-padding border-none rounded transition ease-in-out m-0 placeholder-teal-900"
+              placeholder="Password"
+            />
+            {errors.password?.type === "required" && (
+              <span className="text-red-600">{errors.password.message}</span>
+            )}
+            {errors.password?.type === "pattern" && (
+              <span className="text-red-600">{errors.password.message}</span>
+            )}
+            <MdOutlineVisibility
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute text-slate-700 right-0 top-[6px] mr-4 cursor-pointer"
+              size={28}
+            />
           </div>
+          }
           <div className="mb-2">
             <Link to={"#/"}>Forgot Password?</Link>
           </div>
@@ -83,4 +149,4 @@ export default function Login() {
       </div>
     </div>
   );
-}
+};
--- src/Pages/Authentication/Register.tsx
+++ src/Pages/Authentication/Register.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm,SubmitHandler } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
+import Loader from "../../Components/Loader/Loader";
+import { MdOutlineVisibilityOff, MdOutlineVisibility } from "react-icons/md";
 
 type FormValues = {
   firstName: string;
@@ -11,11 +13,33 @@ type FormValues = {
 
 export default function Register() {
   const { register, formState: { errors }, handleSubmit } = useForm<FormValues>();
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
+  const [loader, setLoader]= useState(false);
+
 
   const onSubmit: SubmitHandler<FormValues> = (data) => {
-    navigate("/login")
+    const url = "https://juice-bar-server.vercel.app/api/v1/signup";
+    setLoader(true)
+      fetch(url,{
+        method: "POST",
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      .then(res=>res.json())
+      .then(data=>{
+        if(data.status ==="success"){
+        setLoader(false)
+        navigate("/login")
+        }
+      })
   };
+
+  if(loader){
+    return <Loader/>
+  }
   
   return (
     <div className="bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED] pt-8 pb-8 flex justify-center">
@@ -96,14 +120,16 @@ export default function Register() {
               <span className="text-red-600">{errors.email.message}</span>
             )}
           </div>
-          <div className="mb-6">
+          {showPassword ?
+          <div className="relative mb-6">
             <input
               type="password"
               {...register("password", {
                 required: { value: true, message: "Password is required!" },
-                minLength: {
-                  value: 6,
-                  message: "Please provide Strong password!",
+                pattern: {
+                  value:
+                    /^(?=.*[a-z]{3,})(?=.*[A-Z])(?=.*\d)(?=.*[^\w\d])(.{6,})$/,
+                  message: "Enter strong password with minLength six,minNumbers one,minUppercase one, minSymbols one. ",
                 },
               })}
               className="block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED] bg-clip-padding border-none rounded transition ease-in-out m-0 placeholder-teal-900"
@@ -112,10 +138,42 @@ export default function Register() {
             {errors.password?.type === "required" && (
               <span className="text-red-600">{errors.password.message}</span>
             )}
-            {errors.password?.type === "minLength" && (
+            {errors.password?.type === "pattern" && (
               <span className="text-red-600">{errors.password.message}</span>
             )}
+            <MdOutlineVisibilityOff
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute text-slate-700 right-0 top-[6px] mr-4 cursor-pointer"
+              size={28}
+            />
+          </div>:
+          <div className="relative mb-6">
+            <input
+              type="text"
+              {...register("password", {
+                required: { value: true, message: "Password is required!" },
+                pattern: {
+                  value:
+                    /^(?=.*[a-z]{3,})(?=.*[A-Z])(?=.*\d)(?=.*[^\w\d])(.{6,})$/,
+                  message: "Enter strong password with minLength six,minNumbers one,minUppercase one, minSymbols one. ",
+                },
+              })}
+              className="block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED] bg-clip-padding border-none rounded transition ease-in-out m-0 placeholder-teal-900"
+              placeholder="Password"
+            />
+            {errors.password?.type === "required" && (
+              <span className="text-red-600">{errors.password.message}</span>
+            )}
+            {errors.password?.type === "pattern" && (
+              <span className="text-red-600">{errors.password.message}</span>
+            )}
+            <MdOutlineVisibility
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute text-slate-700 right-0 top-[6px] mr-4 cursor-pointer"
+              size={28}
+            />
           </div>
+          }
           <button
             type="submit"
             className="block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED] rounded transition ease-in-out m-0 placeholder-teal-900"
