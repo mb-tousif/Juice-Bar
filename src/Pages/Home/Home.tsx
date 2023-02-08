4b8b791b6fbefa0b51625227c4141f00ec8a0649
mb-tousif <meta.block.tousif@gmail.com>
2023-02-08 19:15:07 +0600

    Site name updated

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="text-center bg-gradient-to-b from-[#c1dfc4] to-[#ADCDED]">
       <h1 className="text-xl sm:text-2xl md:text-3xl text-center font-bold text-lime-800 pt-4 pb-4">
-        Welcome To Juice Bar 🥤!
+        Welcome To Fresh Juice Bar 🥤!
       </h1>
       <HeroSection />
       <h1 className="text-xl mb-4 sm:text-2xl md:text-3xl text-center font-bold text-lime-800 pb-4">
